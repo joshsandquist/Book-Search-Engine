df9c6efe867cf84b3ec4b7831c53c499fa165055
Josh Sandquist <joshdsand@gmail.com>
2023-07-07 16:03:51 -0700

    added login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,6 +15,31 @@ const resolvers = {
     },
 
     Mutation: {
+        // Mutation to add a user taking in username, email, and password as args
+        addUser: async (parent, { username, email, password }, context) => {
+            const user = await User.create({ username, email, password});
+            const token = signToken(user);
+            return {token, user}
+        },
+
+        // Login mutation that takes in a email and password as args
+        // Searching for user by email
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+        //Throw a general error if no email found
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials!');
+            }
+        //Using bcrypt .isCorrectPassword function to compare stored hashed password with inputted password
+            const correctPw = await user.isCorrectPassword(password);
+        //Throw error if incorect password
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+        //Sign JWT token
+            const token = signToken(user);
+            return { token, user };
+          },
 
     }
 }
